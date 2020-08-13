513c9db53c7a8870af0f9b88d73c3e51a389acf5
root <root@44f59091f6ad>
2020-08-13 01:31:26 +0000

    polymorph ip last step

--- src/_03_polymorphs/MovingMorph.java
+++ src/_03_polymorphs/MovingMorph.java
@@ -1,13 +1,28 @@
 package _03_polymorphs;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 
 public class MovingMorph extends Polymorph {
+int move = 0;
+Random rand = new Random();
+	MovingMorph(int x, int y, int width, int height) {
+		super(x, y, 50, 50);
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
 	public void draw(Graphics g) {
 		// TODO Auto-generated method stub
+		Color color = new Color(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255));
+		g.setColor(color);
+		g.fillRect(getX(), getY(), getWidth(), getHeight());
+	}
+	public void update() {
 		
+		setY(rand.nextInt(PolymorphWindow.HEIGHT));
+		setX(rand.nextInt(PolymorphWindow.WIDTH));
 	}
 
 }
--- src/_03_polymorphs/PolymorphWindow.java
+++ src/_03_polymorphs/PolymorphWindow.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -16,10 +17,7 @@ public class PolymorphWindow extends JPanel implements ActionListener{
     
     private JFrame window;
     private Timer timer;
-    
-    Polymorph bluePoly;
-    Polymorph redPoly;
-    
+    ArrayList<Polymorph> polys = new ArrayList<Polymorph>();
     public static void main(String[] args) {
    	 new PolymorphWindow().buildWindow();
     }
@@ -27,13 +25,11 @@ public class PolymorphWindow extends JPanel implements ActionListener{
     public void buildWindow(){
    	 window = new JFrame("IT'S MORPHIN' TIME!");
    	 window.add(this);
-   	 window.getContentPane().setPreferredSize(new Dimension(500, 500));
+   	 window.getContentPane().setPreferredSize(new Dimension(WIDTH, HEIGHT));
    	 window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
    	 window.pack();
    	 window.setVisible(true);
-   	 
-   	 bluePoly = new BluePolymorph(50, 50);
- redPoly = new RedMorph(100, 100);
+   	 polys.add(new RedMorph(1, 1));
    	 
    	 timer = new Timer(1000 / 30, this);
    	 timer.start();
@@ -42,17 +38,14 @@ public class PolymorphWindow extends JPanel implements ActionListener{
     public void paintComponent(Graphics g){
     //draw background
    	 g.setColor(Color.LIGHT_GRAY);
-   	 g.fillRect(0, 0, 500, 500);
+   	 g.fillRect(0, 0, WIDTH, HEIGHT);
    	
    	 //draw polymorph
-   	 bluePoly.draw(g);
-   	 redPoly.draw(g);
+   	 
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
    	 repaint();
-   	 bluePoly.update();
-   	 redPoly.update();
     }
 }
